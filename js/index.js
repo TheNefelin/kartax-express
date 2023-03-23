2749b5e4c054ceb105bfb05df7c51cde3c12bdeb
Nefelin <flcarmonac@yahoo.com>
2023-03-23 09:31:57 -0300

    Update

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ window.onload = () => {
 };
 
 function inicializar() {
-    const idNegocio = 1;
+    // const idNegocio = 1;
     // // se renderiza el fondo en modo paralax
     // const body = document.querySelector("body");
     // body.classList.add("paralax");
@@ -18,7 +18,7 @@ function inicializar() {
     // const navbar = document.querySelector("nav");
     // navbar.appendChild(renderNavBar());
 
-    const header = document.querySelector("header");
+    // const header = document.querySelector("header");
     // header.appendChild(renderLoading());
 
     // const main = document.querySelector("main");
@@ -27,22 +27,22 @@ function inicializar() {
     // const footer = document.querySelector("footer");
     // footer.appendChild(renderLoading());
 
-    const api = new Api();
+    // const api = new Api();
 
-    Promise.all([
-        api.getNegocioById(idNegocio).then(data => data),
-        api.getTipoAlimByIdNegocio(idNegocio).then(data => data),
-        api.getItemCategByIdTipoAlimento(1).then(data => data),
-        // api.getItemByIdCateg(2).then(data => data),
-        api.getItemByIdNegocio(idNegocio).then(data => data),
-        api.getLinksGrpAll().then(data => data),
-        api.getLinksAll().then(data => data),
-    ]).then(arr => {
+    // Promise.all([
+    //     api.getNegocioById(idNegocio).then(data => data),
+    //     api.getTipoAlimByIdNegocio(idNegocio).then(data => data),
+    //     api.getItemCategByIdTipoAlimento(1).then(data => data),
+    //     // api.getItemByIdCateg(2).then(data => data),
+    //     api.getItemByIdNegocio(idNegocio).then(data => data),
+    //     api.getLinksGrpAll().then(data => data),
+    //     api.getLinksAll().then(data => data),
+    // ]).then(arr => {
         // header.innerHTML = "";
         // header.appendChild(renderSlider(arr[1]));
-        main.innerHTML = "";
-        main.appendChild(renderAcordion(arr[2], arr[3]));
-        footer.innerHTML = "";
-        footer.appendChild(renderFooterLinks(arr[4], arr[5]));
-    });
+        // main.innerHTML = "";
+        // main.appendChild(renderAcordion(arr[2], arr[3]));
+        // footer.innerHTML = "";
+        // footer.appendChild(renderFooterLinks(arr[4], arr[5]));
+    // });
 };
