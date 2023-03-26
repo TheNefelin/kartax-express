d2cef9fc41230a9a4cc6a3d752b3f548a573bba0
TheNefelin <metalflap@gmail.com>
2023-03-26 12:30:10 -0300

    Update

--- routes/routes.js
+++ routes/routes.js
@@ -5,18 +5,22 @@ const myRoutes = Router();
 export default myRoutes;
 
 myRoutes.get("/", (req, res) => {
-    fn.dataKartax(1).then(data => {
-        res.render("kartax", data);
-    }).catch(() => {
-        res.render("error");
-    });
+    res.render("login");
 });
 
-myRoutes.get("/:id", (req, res) => {
-    const id = isNaN() ? 1 : req.params.id;
-    fn.dataKartax(id).then(data => {
-        res.render("kartax", data);
-    }).catch(() => {
-        res.render("error");
-    });
-});
+// myRoutes.get("/", (req, res) => {
+//     fn.dataKartax(1).then(data => {
+//         res.render("kartax", data);
+//     }).catch(() => {
+//         res.render("error");
+//     });
+// });
+
+// myRoutes.get("/:id", (req, res) => {
+//     const id = isNaN() ? 1 : req.params.id;
+//     fn.dataKartax(id).then(data => {
+//         res.render("kartax", data);
+//     }).catch(() => {
+//         res.render("error");
+//     });
+// });
