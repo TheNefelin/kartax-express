89d3fd79401547db23aec6e1704eb7e3181487dc
Nefelin <flcarmonac@yahoo.com>
2023-04-21 10:07:31 -0400

    update

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,2 @@
-const API_LINK = "https://kartax-api-production.up.railway.app";
-// const API_LINK = "http://localhost:3001"
+//const API_LINK = "https://kartax-api-production.up.railway.app";
+const API_LINK = "http://localhost:3001"
