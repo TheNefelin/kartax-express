a6ee6f39462f6110b3aaf2efc9512d67d2a02c77
Nefelin <flcarmonac@yahoo.com>
2023-04-25 11:28:38 -0400

    update

--- README.md
+++ README.md
@@ -12,10 +12,7 @@
 ```
 npm install express
 npm install hbs
-npm install body-parser
-npm install mysql2
 npm install pg
-npm install express-session
 ```
 
 ## Rúbrica
--- index.js
+++ index.js
@@ -3,22 +3,14 @@ import hbs from "hbs";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 import routes from "./routes/routes.js";
-import bodyParser from 'body-parser';
-import session from "express-session";
 
 const app = express();
 
 hbs.registerPartials(join(dirname(fileURLToPath(import.meta.url)), "/views/partials"));
 app.set("view engine", "hbs");
 app.use(express.static("public"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(session({
-    secret: 'esmerilemelo',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true }
-}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 app.listen(3000, (req, res) => {
     console.log("Kartax App Running on 3000!!!");
