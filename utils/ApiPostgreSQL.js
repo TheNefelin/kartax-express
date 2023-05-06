4a86411ce495d1f052b3e0b861cf00c92fe9703f
Nefelin <flcarmonac@yahoo.com>
2023-05-05 21:52:06 -0400

    update

--- utils/ApiPostgreSQL.js
+++ utils/ApiPostgreSQL.js
@@ -1,51 +1,55 @@
 import dotenv from 'dotenv'
 dotenv.config();
-const url = process.env.API_LINK;
-
-//const url = "http://localhost:3001";
-//const url = "https://kartax-api-production.up.railway.app";
 
 export default class ApiPostgreSQL {
-    constructor() {};
+    #url;
+    constructor() {
+        //this.#url = "http://localhost:3001";
+        //this.#url = "https://kartax-api-production.up.railway.app";
+        this.#url = process.env.API_LINK;
+    };
+    getUrl() {
+        return this.#url
+    }
     // publico --------------------------------------------------------
     async registrarUsuario(txtNombres, txtApellidos, txtUser, txtEmail, txtPass1, txtPass2) {
-        return await post(`${url}/registrar-usuario`, { txtNombres, txtApellidos, txtUser, txtEmail, txtPass1, txtPass2 });
+        return await post(`${this.#url}/registrar-usuario`, { txtNombres, txtApellidos, txtUser, txtEmail, txtPass1, txtPass2 });
     };
     async iniciarSesion(txtUser, txtPass) {
-        return await post(`${url}/iniciar-sesion`, { txtUser, txtPass });
+        return await post(`${this.#url}/iniciar-sesion`, { txtUser, txtPass });
     };
     async validarToken(token) {
-        return await get(`${url}/token/${ token }`);
+        return await get(`${this.#url}/token/${ token }`);
     }
     async getNegocio_ByIdMesa(id) {
-        return await get(`${url}/negocio/idMesa/${ id }`);
+        return await get(`${this.#url}/negocio/idMesa/${ id }`);
     };
     async getTipoAlim_ByIdNegocio(id) {
-        return await get(`${url}/tipo-alimento/${ id }`);
+        return await get(`${this.#url}/tipo-alimento/${ id }`);
     };
     // privado --------------------------------------------------------
     async getAdmin(usuario, token) {
-        return await get(`${url}/admin/${ usuario }&${ token }`);
+        return await get(`${this.#url}/admin/${ usuario }&${ token }`);
     };
     // negocios
     async getAdminNegocios(usuario, token) {
-        return await get(`${url}/admin/negocios/${ usuario }&${ token }`);
+        return await get(`${this.#url}/admin/negocios/${ usuario }&${ token }`);
     };
     async postAdminNegocios(usuario, token, obj) {
-        return await post(`${url}/admin/negocios`, { usuario, token, data: obj });
+        return await post(`${this.#url}/admin/negocios`, { usuario, token, data: obj });
     };
     async putAdminNegocios(usuario, token, obj) {
-        return await put(`${url}/admin/negocios`, { usuario, token, data: obj });
+        return await put(`${this.#url}/admin/negocios`, { usuario, token, data: obj });
     };
     // usuarios
     async getAdminUsuarios(usuario, token) {
-        return await get(`${url}/admin/usuarios/${ usuario }&${ token }`);
+        return await get(`${this.#url}/admin/usuarios/${ usuario }&${ token }`);
     };
     async postAdminUsuarios(usuario, token) {
-        return await post(`${url}/admin/usuarios`, { usuario, token, data: obj });
+        return await post(`${this.#url}/admin/usuarios`, { usuario, token, data: obj });
     };
     async putAdminUsuarios(usuario, token) {
-        return await put(`${thisurl}/admin/usuarios`, { usuario, token, data: obj });
+        return await put(`${this.#url}/admin/usuarios`, { usuario, token, data: obj });
     };
     // ----------------------------------------------------------------
     async getLinksCateg_All() {
@@ -61,7 +65,6 @@ async function get(url) {
         const res = await fetch(url);
         return await res.json();
     } catch(err) {
-        console.log(url)
         console.log(`Error: en la conexion a la API (ApiPostgreSQL) Detalle: ${ err }`)
         return [];
     };
